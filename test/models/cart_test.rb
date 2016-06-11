4f2018decd30c49b972242d8a9e45a9037207ee9
metatrontu@gmail.com <metatrontu@gmail.com>
2016-06-11 10:09:39 +0800

    Serialize and let cart can build from hash, and also test the cart model

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -10,18 +10,42 @@ class CartTest < MiniTest::Test
 
   def test_adds_up_in_quantity
     cart = Cart.new
-    3.times { cart.add_item 1}
+    4.times { cart.add_item 1}
 
-    assert_equal cart.items.length, 1
-    assert_equal cart.items.first.quantity, 3
+    assert_equal 1, cart.items.length
+    assert_equal 4, cart.items.first.quantity
   end
 
   def test_retrieves_products
-  	product = Product.create! name: "Tomato", price: 1
+  	product = Product.create! name: "Watermelon", price: 1
 
     cart = Cart.new
     cart.add_item product.id
 
     assert_kind_of Product, cart.items.first.product
+  end
+
+  def test_serializes_to_hash
+  	cart = Cart.new
+  	cart.add_item 1
+
+  	assert_equal cart.serialize, session_hash
+  end
+
+
+  def test_builds_from_hash
+  	cart = Cart.build_from_hash session_hash
+  	assert_equal 1, cart.items.first.product_id
+  end
+
+  private
+  def session_hash
+  	{
+  		"cart" => {
+  			"items" => [
+  			  { "product_id" => 1, "quantity" => 1}
+  			]
+  		}
+  	}
   end
 end
